8172b428e18b72f68c267fe81f2271c969645a7f
almostgone47 <almostgone47@hotmail.com>
2023-08-04 13:17:27 +0800

    add documentation for  the hash table class

--- HashTable.cpp
+++ HashTable.cpp
@@ -4,6 +4,11 @@
 
 #include "HashTable.h"
 
+//Name:   HashTable
+//Desc:   Constructor for HashTable class.
+//input:  none
+//output: none
+//return: none
 HashTable::HashTable() {
     tableSize = 9973;
     arr = new LinkedList[tableSize];
@@ -12,6 +17,11 @@ HashTable::HashTable() {
     }
 }
 
+//Name:   HashTable
+//Desc:   Constructor for HashTable class with custom table size.
+//input:  An integer for the size of the table being created.
+//output: none
+//return: none
 HashTable::HashTable(int hashTableSize) {
     tableSize = hashTableSize;
     arr = new LinkedList[tableSize];
@@ -20,6 +30,11 @@ HashTable::HashTable(int hashTableSize) {
     }
 }
 
+//Name:   HashTable
+//Desc:   Copy constructor for HashTable class.
+//input:  none
+//output: none
+//return: none
 HashTable::HashTable(const HashTable& table) {
     tableSize = table.tableSize;
     arr = new LinkedList[tableSize];
@@ -29,6 +44,11 @@ HashTable::HashTable(const HashTable& table) {
     length = table.length;
 }
 
+//Name:   operator=
+//Desc:   Redefines = to perform deep copy.
+//input:  Reference to the linked list to copy from.
+//output: none
+//return: A reference to the linked list that used the method to copy.
 const HashTable& HashTable::operator=(const HashTable& table) {
     if (this == &table) {
         return *this;
@@ -44,6 +64,11 @@ const HashTable& HashTable::operator=(const HashTable& table) {
     return *this;
 }
 
+//Name:   ~HashTable
+//Desc:   Destructor for HashTable class.
+//input:  none
+//output: none
+//return: none
 HashTable::~HashTable() {
     delete [] arr;
 }
@@ -57,23 +82,43 @@ bool HashTable::isEmpty() {
     return length == 0;
 }
 
+//Name:   insert
+//Desc:   Uses a hash of the id passed for an index in the array holding the hash table buckets.
+//input:  none
+//output: none
+//return: none
 void HashTable::insert(char *id, Person person) {
     unsigned int index = hashFunction(id);
     arr[index].insertFront(person);
     length++;
 }
 
+//Name:   remove
+//Desc:   Uses the passed in id get the hash used as the index in the hash table array and calls the LinkedList removeById method.
+//input:  A string for the id of the person object to be removed.
+//output: none
+//return: none
 void HashTable::remove(char id[]) {
     unsigned int index = hashFunction(id);
     arr[index].removeById(id);
     length--;
 }
 
+//Name:   lookup
+//Desc:   Uses the passed in id get the hash used as the index in the hash table array and calls the LinkedList searchById method.
+//input:  A string for the id of the person object to search for.
+//output: none
+//return: A pointer to a Person object searched for.
 const Person * HashTable::lookup(char id[]) {
     unsigned int index = hashFunction(id);
     return arr[index].searchById(id);
 }
 
+//Name:   hashFunction
+//Desc:   Creates hash from the passed in Person id to be used as the index in the hash table array of buckets.
+//input:  A string for the id of the person object to be indexed.
+//output: none
+//return: A hash that is no larger than the tableSize.
 unsigned int HashTable::hashFunction(const char* key) {
     unsigned int hash = 0;
     unsigned int mod = tableSize;
@@ -86,6 +131,11 @@ unsigned int HashTable::hashFunction(const char* key) {
     return hash;
 }
 
+//Name:   dumpTable
+//Desc:   Loops through the hash table array and prints every item in the hash table.
+//input:  none
+//output: The details for each of the Person objects in the hash table.
+//return: none
 void HashTable::dumpTable() {
     for (int i = 0; i < tableSize; i++) {
         arr[i].printList();
